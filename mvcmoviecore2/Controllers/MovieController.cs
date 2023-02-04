ee496202903d53096eeddfe8d69d2f16abb4560d
dkdavis11 <dkdavis11@yahoo.com>
2023-02-04 08:18:41 -0600

    add dbconnection

--- mvcmoviecore2/Controllers/MovieController.cs
+++ mvcmoviecore2/Controllers/MovieController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,17 +15,26 @@ namespace mvcmoviecore2.Controllers
     {
 
         private IConfiguration configuration;
+        public static string dbConn2 = null;
 
         public MovieController(IConfiguration iConfig)
         {
             configuration = iConfig;
+            
         }
 
 
         [HttpGet]
         public IActionResult GetUsers()
         {
-            string dbConn2 = configuration.GetValue<string>("ConnectionStrings:Default");
+          if (dbConn2== null)
+            dbConn2 = configuration.GetValue<string>("ConnectionStrings:Default");
+
+            using var connection = new MySqlConnection(dbConn2);
+            connection.OpenAsync();
+            using var command = new MySqlCommand("SELECT field FROM movie;", connection);
+
+            //string dbConn = configuration.GetSection("MySettings").GetSection("DbConnection").Value;
             var list = new List<string>();
             list.Add("John");
             list.Add("Doe");
