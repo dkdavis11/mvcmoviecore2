840bca150e113303d60bd37142e47adeb383d81f
dkdavis11 <dkdavis11@yahoo.com>
2023-02-03 19:01:58 -0600

    push changes

---
+++ mvcmoviecore2/Controllers/MovieController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace mvcmoviecore2.Controllers
+{
+    public class MovieController : Controller
+    {
+
+        private IConfiguration configuration;
+
+        public MovieController(IConfiguration iConfig)
+        {
+            configuration = iConfig;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            string dbConn2 = configuration.GetValue<string>("ConnectionStrings:Default");
+            var list = new List<string>();
+            list.Add("John");
+            list.Add("Doe");
+            return Ok(list);
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ mvcmoviecore2/Models/MySettingsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace mvcmoviecore2.Models
+{
+	public class MySettingsModel
+	{
+
+        public string DbConnection { get; set; }
+        public string Email { get; set; }
+        public string SMTPPort { get; set; }
+
+        public MySettingsModel()
+		{
+		}
+	}
+}
+
