cfe5e78d980d6aa529af06607bf628e6a68e0cf6
dkdavis11 <dkdavis11@yahoo.com>
2023-02-11 16:45:50 -0600

    changes for singleton

--- mvcmoviecore2/Controllers/MovieController.cs
+++ mvcmoviecore2/Controllers/MovieController.cs
@@ -58,6 +58,85 @@ namespace mvcmoviecore2.Controllers
 
             return Ok(list);
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Details()
+        {
+            if (dbConn2 == null)
+                dbConn2 = configuration.GetValue<string>("ConnectionStrings:Default");
+
+            using var connection = new MySqlConnection(dbConn2);
+            await connection.OpenAsync();
+            using var command = new MySqlCommand("SELECT * FROM movies;", connection);
+
+            //string dbConn = configuration.GetSection("MySettings").GetSection("DbConnection").Value;
+            using var reader = await command.ExecuteReaderAsync();
+            
+            var list = new List<MovieModel>();
+            while (await reader.ReadAsync())
+            {
+                Console.WriteLine(reader.GetInt16(0));
+                Console.WriteLine(reader.GetString(1));
+                Console.WriteLine(reader.GetDateTime(2));
+                Console.WriteLine(reader.GetString(3));
+                Console.WriteLine(reader.GetDecimal(4));
+                MovieModel model = new MovieModel();
+                model.Id = reader.GetInt16(0);
+                model.Title = reader.GetString(1);
+                model.ReleaseDate = reader.GetDateTime(2);
+                model.Genre = reader.GetString(3);
+                model.Price = reader.GetDecimal(4);
+
+                list.Add(model);
+
+            }
+
+            await connection.CloseAsync();
+            return View(list);
+        }
+
+        // GET: Movies/Details/5
+        [HttpGet]
+        public async Task<IActionResult> DetailsSingle(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+
+            if (dbConn2 == null)
+                dbConn2 = configuration.GetValue<string>("ConnectionStrings:Default");
+
+            using var connection = new MySqlConnection(dbConn2);
+            await connection.OpenAsync();
+            using var command = new MySqlCommand("SELECT * FROM movies where Id ="+Id +";", connection);
+            using var reader = await command.ExecuteReaderAsync();
+            //MovieModel var
+            var movie = await reader.ReadAsync();
+            MovieModel model = new MovieModel();
+            model.Id = reader.GetInt16(0);
+            model.Title = reader.GetString(1);
+            model.ReleaseDate = reader.GetDateTime(2);
+            model.Genre = reader.GetString(3);
+            model.Price = reader.GetDecimal(4);
+
+            //var movie = await reader
+            //    .FirstOrDefaultAsync(m => m.Id == id);
+            //if (movie == null)
+            //{
+            //    return NotFound();
+            //}
+            ViewBag.MovieModel = model;
+            return View();
+        }
+
+
+
+
+
+
         // GET: /<controller>/
         public IActionResult Index()
         {
